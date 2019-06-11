5c3ef8e73f50ce114d540f6edda3db5ce76383cc
victor ordax <victorordaxantolin@gmail.com>
2019-06-11 16:58:46 +0200

    muestra los personajes de cada comic con su enlace

---
+++ src/components/chataracterInComic.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+class CharactersInComic extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            personajes:[]
+        }
+
+        axios.get(props.ApiPersonajes).then((res) => {
+            let  personajes = res.data.data.results;
+            console.log("personajes",personajes);
+            this.setState({
+                personajes
+            }) 
+           
+        })
+    //    console.log("Buenas tardes",this.state.personajes);
+    }
+    
+    render(){
+        return <div>
+            
+                {this.state.personajes.map((item, i) => <div key={i}><Link to={"/detalleCharacter/" + item.id}> {item.name}</Link> </div>)}
+        </div>
+    }
+
+}
+
+export default CharactersInComic
--- src/components/detailComic.jsx
+++ src/components/detailComic.jsx
@@ -1,24 +1,53 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
 import {Link} from 'react-router-dom';
+import CharactersInComic from './chataracterInComic'
 const API="https://gateway.marvel.com:443/v1/public/comics/";
 const API2="?apikey=26d7528942f2d504c462e42835bd7313"
 
 class DetailComic extends Component{
+    
     constructor(props){
         super(props);
         this.state = {
-            comic: null
+            comic: null,
+            ApiPersonajes:"",
+            personajes:[]
         }
-
 
         this.comicId = props.match.params.id;
         Axios.get(API  + this.comicId + API2).then((res) => {
             let  comic = res.data.data.results[0];
+            console.log(comic);
+            let ApiPersonajes=res.data.data.results[0].characters.collectionURI
+            ApiPersonajes=ApiPersonajes + API2;
+
             this.setState({
-                comic
-            })
+                comic,
+                ApiPersonajes
+            }) 
+
         })
+        
+        
+        
+    //     let personajes= async () =>{
+    //         let all=await Axios.get(this.state.ApiPersonajes);
+    //         return all.data.results
+    //    }
+    //    this.setState({
+    //        personajes
+    //     })
+        // console.log(personajes);
+        
+        
+            // await Axios.get(this.state.ApiPersonajes).then((res) => {
+            //     let  personajes = res;
+            //     console.log("los personajes",personajes)
+            //     this.setState({
+            //         personajes
+            //     })
+            // })
     }
 
     render(){
@@ -26,10 +55,12 @@ class DetailComic extends Component{
         if(this.state.comic){
             return <div>
                 <Link to="/">Go home</Link>
+                <p>{this.state.ApiPersonajes}</p>
+                <p>{this.state.personajes}</p>
                 <h1>{this.state.comic.name}</h1>
                 <h1>{this.state.comic.id}</h1>
-                {console.log("Hola",this.state.comic.id)}
                 <img src={this.state.comic.thumbnail.path +"."+ this.state.comic.thumbnail.extension} alt=""/>
+                <CharactersInComic ApiPersonajes={this.state.ApiPersonajes}></CharactersInComic>
             </div>
         }else{
             return <div>
